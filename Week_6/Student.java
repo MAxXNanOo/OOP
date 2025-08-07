b4baa99ceeb6d9612366f8e57460e618c1b70470
MAxXNanOo <maxxnanoo@gmail.com>
2025-08-08 00:45:29 +0700

    OOP

---
+++ Week_6/Student.java
@@ -0,0 +1,34 @@
+public class Student {
+    private String name;
+    private int id;
+    private double gpa;
+
+    public Student(int id, String name, double gpa){
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public String getName(){
+        
+    }
+
+    public double gatGPA(){
+
+    }
+
+    public int id(){
+
+    }
+
+    public void setName(){
+
+    }
+
+    public void toString(){
+
+    }
+
+
+
+}
---
+++ Week_6/Undergrad.java
@@ -0,0 +1,20 @@
+public class Undergrad extends Student{
+    private String year;
+
+    public Undergrad(int id, String name, double gpa, String year){
+        
+        this.year = year;
+    }
+
+    public void setYear(String year){
+        
+    }
+
+    public String getYear(){
+
+    }
+
+    public void toString(){
+
+    }
+}
