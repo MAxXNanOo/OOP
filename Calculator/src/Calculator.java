bc08dbc1fbd74faf861fd211911f4078610efec6
MAxXNanOo <maxxnanoo@gmail.com>
2025-09-26 02:30:39 +0700

    OOP

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -2,17 +2,21 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Stack;
+
 
 public class Calculator extends JFrame implements ActionListener{
     private JButton [] button = new JButton[20];
 
-    private String [] stringbutton = {"C", "CE" ,"/" , "*" ,
-            "7" , "8" , "9" , "-" ,
-            "4" , "5" , "6" , "+" ,
-            "1" , "2" , "3" , "=" ,
-            "0", "0" , "." , "=" };
+    private String [] stringbutton = {
+                                        "C", "CE" ,"/" , "*" ,
+                                        "7" , "8" , "9" , "-" ,
+                                        "4" , "5" , "6" , "+" ,
+                                        "1" , "2" , "3" , "=" ,
+                                        "0", "0" , "." , "="    };
 
-    JLabel labelbefore = new JLabel("");
+
+    JLabel labelbefore = new JLabel(" ");
     JLabel labelsum = new JLabel("0");
 
     Color grayColor = new Color(40, 40, 40);
@@ -75,23 +79,44 @@ public class Calculator extends JFrame implements ActionListener{
                 button[Index] = new JButton(stringbutton[Index]);
                 setButtonStyle(button[Index]);
 
-                if(stringbutton[Index].matches("[0-9]")) {
+                if(stringbutton[Index].matches("[1-9]")) {
                     button[Index].addActionListener(new ActionListener() {
-
+                        int IndexButton = Index;
                         @Override
                         public void actionPerformed(ActionEvent e) {
-                            if(labelsum.getText().length() == 1  &&  labelsum.getText().equals("0")) {
-                                labelsum.setText( button[Index].getText());
-                            }
-                            else {
-                                labelbefore.setText(labelbefore.getText() + button);
-                                labelbefore.setText(labelsum.getText() + button);
-                            }
-
-
+                                if(labelsum.getText().matches("[0,+,--,*,/]")) {
+                                    labelsum.setText("");
+                                }
+                                labelbefore.setText(labelbefore.getText() + button[IndexButton].getText());
+                                labelsum.setText(labelsum.getText() + button[IndexButton].getText());
                         }
 
                     });
+                }
+                else if(stringbutton[Index].equals("C")) {
+                    button[Index].addActionListener(e -> {
+                        labelbefore.setText("");
+                        labelsum.setText("0");
+                    });
+                }
+                else if(stringbutton[Index].equals("CE")) {
+                    button[Index].addActionListener(e -> {
+                        String tmp = labelbefore.getText().replace(labelsum.getText(), "");
+                        labelbefore.setText(tmp);
+                        labelsum.setText("0");
+                    });
+                }
+                else{
+                    int IndexButton = Index; // ล็อกค่า Index ไว้ก่อน
+
+                    button[IndexButton].addActionListener(e -> {
+                        if(!labelsum.getText().matches("[0,+,--,*,/]") ) {
+                            String tmp = button[IndexButton].getText();
+                            labelbefore.setText(labelbefore.getText() + tmp);
+                            labelsum.setText(tmp);
+                        }
+                    });
+
                 }
 
                 pane.add(button[Index], c);
@@ -105,12 +130,34 @@ public class Calculator extends JFrame implements ActionListener{
         c.gridheight = 1;
         pane.add(button[16] = new JButton(stringbutton[16]), c);
         setButtonStyle(button[16]);
+        button[16].addActionListener(e -> {
+            if(labelsum.getText().matches("[0,+,--,*,/]")) {
+                labelsum.setText("0");
+            }
+            else{
+                if(labelsum.getText().equals("0")) {
+                    labelsum.setText("");
+                }
+                labelsum.setText(labelsum.getText() + button[16].getText());
+                labelbefore.setText(labelbefore.getText() + button[16].getText());
+            }
+        });
 
         c.gridx = 2;
         c.gridy = 6;
         c.gridwidth = 1;
         pane.add(button[18] = new JButton(stringbutton[18]), c);
         setButtonStyle(button[18]);
+        button[18].addActionListener(e -> {
+            if(labelsum.getText().matches("0")) {
+                labelbefore.setText(labelbefore.getText() + "0.");
+            }
+            else{
+                labelbefore.setText(labelbefore.getText() + ".");
+            }
+
+            labelsum.setText(labelsum.getText() + button[18].getText());
+        });
 
         c.gridx = 3;
         c.gridy = 5;
@@ -119,14 +166,52 @@ public class Calculator extends JFrame implements ActionListener{
         setButtonStyle(button[19]);
         button[19].setForeground(grayColor);
         button[19].setBackground(new Color(90, 180, 250));
-        button[19].addActionListener(new ActionListener() {
+        button[19].addActionListener(e -> {
+            char [] arr = labelbefore.getText().toCharArray();
+            char [] tmp = new char[arr.length];
+            String [] arr2 = new String[arr.length];
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                labelbefore.setText(labelbefore.getText() + "nig");
-
+            int Index=-1, sizeNumber = -1;
+            for(int i=0 ; i<arr.length ; i++) {
+                if(arr[i]!='+' && arr[i]!='-' && arr[i]!='*' && arr[i]!='/'){
+                    tmp[++Index] = arr[i];
+                }
+                else{
+                    arr2[++sizeNumber] = new String(tmp).trim();
+                    arr2[++sizeNumber] = new String(String.valueOf(arr[i])).trim();
+                    tmp =  new char[arr.length];
+                    Index = -1;
+                }
             }
+            arr2[++sizeNumber] = new String(tmp).trim();
+
+
+            System.out.printf("\n\n\n\n");
+            for( ; sizeNumber>-1 ; sizeNumber--){
+                System.out.println(arr2[sizeNumber]);
+            }
+
+
+
+            //รอทำ prefix เเละ postfix
+//            double [] numbers = new double[sizeNumber];
+//            int topNumber = -1;
+//            for(int i=0 ; i<=sizeNumber ; i++){
+//                if(arr2[i].matches("[+,--,*,/]")){
+//                    switch(arr2[i]){
+//                        case "+":
+//                            top
+//                    }
+//                }
+//                else{
+//                   numbers[++topNumber] = Double.parseDouble(arr2[i]);
+//                }
+//            }
+
         });
+
+
+
 
 
 
@@ -149,14 +234,6 @@ public class Calculator extends JFrame implements ActionListener{
         button.setFocusPainted(false);
         button.setOpaque(true);
         button.setBackground(new Color(70,70,70));
-//		button.addMouseListener(new java.awt.event.MouseAdapter() {
-//			public void mousePressed(java.awt.event.MouseEvent e) {
-//				button.setBackground(new Color(20,20,20));
-//			}
-//			public void mouseReleased(java.awt.event.MouseEvent e) {
-//				button.setBackground(new Color(70,70,70));
-//			}
-//		});
     }
 
     public static void main(String[] args) {
